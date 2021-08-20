c2f161d7173336f2c5c246ded8c3c3693dc0ac26
deepakcse1784110024 <62210197+deepakcse1784110024@users.noreply.github.com>
2021-08-20 18:12:40 +0530

    added successfully

---
+++ dbcon.php
@@ -0,0 +1,12 @@
+<?php
+  
+   $con=mysqli_connect('localhost','root','','payrollmanagementsystem'); 
+   if($con==false){
+       echo "connection is not stablish";
+   }
+   else{
+       echo "connection is stablish";
+   }
+
+
+?>
